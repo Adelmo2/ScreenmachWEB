8624c65bdbecb117cb64a1246de41f4190bbdae3
Adelmo Ricardo <adelmo.ricardo@sherwin-auto.com.br>
2025-11-28 17:45:18 -0300

    261125

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,8 +1,7 @@
 package br.com.alura.screenmatch.controller;
 
-import br.com.alura.screenmatch.model.Serie;
-import br.com.alura.screenmatch.repository.SerieRepository;
-//import org.hibernate.mapping.List;
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +10,13 @@ import java.util.List;
 
 @RestController
 public class SerieController {
+
     @Autowired
-    private SerieRepository repositorio;
+    private SerieService servico;
 
     @GetMapping("/series")
     //public String obterSeries() {
-    public List<Serie> obterSeries() {
-        return repositorio.findAll();
-        //return "Aqui vão ser listadas a séries";
-        //return repositorio.findTop5ByOrderByAvaliacaoDesc();
+    public List<SerieDTO> obterSeries() {
+        return servico.obterTodasSeries();
     }
 }
