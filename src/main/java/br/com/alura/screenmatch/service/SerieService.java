1f6e8eabcd65443faf2e845b5f9418504f7d440f
Adelmo Ricardo <adelmo.ricardo@sherwin-auto.com.br>
2025-12-02 06:16:18 -0300

    02-12-2025

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,20 @@ public class SerieService {
     private SerieRepository repositorio;
 
     public List<SerieDTO> obterTodasSeries() {
-        return repositorio.findAll()
-            .stream()
-            .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getSinopse()))
-            .collect(Collectors.toList());
+        return converteDados(repositorio.findAll());
+//        return repositorio.findAll()
+//            .stream()
+//            .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getSinopse()))
+//            .collect(Collectors.toList());
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    private List<SerieDTO>  converteDados(List<Serie> series)  {
+        return  series.stream()
+        .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getSinopse()))
+        .collect(Collectors.toList());
     }
 }
